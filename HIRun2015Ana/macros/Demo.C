e07787344373234b21cd142b4092c0bce062fc87
KongTu <kongkong@pb-d-128-141-112-10.cern.ch>
2015-12-16 18:19:10 +0100

    Revert "fix getWeight and Demo"
    This reverts commit 290b3e646c09c247ba1da0fd1b12d5a383bd04d9.

--- HIRun2015Ana/macros/Demo.C
+++ HIRun2015Ana/macros/Demo.C
@@ -12,12 +12,8 @@ void Demo(){
 	TrackCorrector3D testload("../rootfile/PbPb_eff_MC_v1.root");//efficiency in 2D and 3D histograms
 	testload.load("HITrackCorrections");//name of the folder
 
-	//double getWeigt(double pt, double eta, double centralitybin, string option)
-	//if option leaves open or "", getWeight returns (1. - fak ) * ( 1. - sec ) / eff  / (1. + mul )
-	//if option is specified with "eff" or "fak" or "sec" or "mul", getWeight only return the corresponding value 
-	
-	
-	cout << "getWeight with all correction " << testload.getWeight(1.0, 1.0, 50) << endl;
-	cout << "getWeight with only fake rate " << testload.getWeight(1.0, 1.0, 50, "fak") << endl;
+	//double getWeigt(pt, eta, centralitybin)
+	//getWeight is calculated as (1. - fak ) * ( 1. - sec ) / eff  / (1. + mul )
+	cout << "getWeight: " << testload.getWeight(1.0, 1.0, 50) << endl;
 
 }
