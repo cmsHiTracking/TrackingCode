b2f916bccf5d56c2199b370581441393d180488f
Kong <kongPro@Kongs-MacBook-Pro.local>
2016-01-10 17:26:06 +0100

    adding filter performance plot

--- pileUpFilter/macros/plotFilterFraction.C
+++ pileUpFilter/macros/plotFilterFraction.C
@@ -4,44 +4,87 @@ using namespace std;
 
 void plotFilterFraction(){
 	
-	TFile* file = new TFile("../rootfile/pielUpFilterEfficiency_MC_2.root");
-	vector<TH1D*> hist = loadingHistogram(file, "ana_PbPb","/Ntrk", 4);
-	TCanvas* c1 = makeCanvas("c1","Filter Fraction");
-	TH1D* base = makeHist("base", "","N^{offline}_{trk}","Fraction accepted", 200, 0,100, kBlack);
+	TFile* file1 = new TFile("../rootfile/pileUpFilterEfficiency_MC_1.root");
+	TFile* file2 = new TFile("../rootfile/pileUpFilterEfficiency_MC_2.root");
 
-	TGraphAsymmErrors * gr[5];
+	vector<TH1D*> hist1 = loadingHistogram(file1, "ana_PbPb","/leadingPt", 4);
+	vector<TH1D*> hist2 = loadingHistogram(file2, "ana_PbPb","/leadingPt", 4);
+
+	TCanvas* c1 = makeMultiCanvas("c1","Filter Fraction", 2,1);
+
+	TH1D* base1 = makeHist("base1", "PYTHIA MB","leading p_{T}","Fraction accepted", 200, 0,30, kBlack);
+	TH1D* base2 = makeHist("base2", "PYTHIA PU = 2","leading p_{T}","Fraction accepted", 200, 0,30, kBlack);
+	
+
+	c1->cd(1);
+
+	TGraphAsymmErrors * gr1[5];
 	gPad->SetTicks();
 	gPad->SetLeftMargin(0.15);
-	
-	base->Draw();
-	gr[0] = new TGraphAsymmErrors();
-	gr[0]->Divide(hist[1], hist[0], "cp");
-	gr[0]->SetMarkerStyle(20);
-	gr[0]->SetLineColor(1);
-	gr[0]->SetMarkerColor(1);
-	gr[0]->Draw("same");
+	gPad->SetBottomMargin(0.13);
 
-	gr[1] = new TGraphAsymmErrors();
-	gr[1]->Divide(hist[2], hist[0], "cp");
-	gr[1]->SetMarkerStyle(20);
-	gr[1]->SetLineColor(2);
-	gr[1]->SetMarkerColor(2);
-	gr[1]->Draw("same");
+	base1->Draw();
+	gr1[0] = new TGraphAsymmErrors();
+	gr1[0]->Divide(hist1[1], hist1[0], "cp");
+	gr1[0]->SetMarkerStyle(20);
+	gr1[0]->SetLineColor(1);
+	gr1[0]->SetMarkerColor(1);
+	gr1[0]->Draw("same");
 
-	gr[2] = new TGraphAsymmErrors();
-	gr[2]->Divide(hist[3], hist[0], "cp");
-	gr[2]->SetMarkerStyle(20);
-	gr[2]->SetLineColor(4);
-	gr[2]->SetMarkerColor(4);
-	gr[2]->Draw("same");
+	gr1[1] = new TGraphAsymmErrors();
+	gr1[1]->Divide(hist1[2], hist1[0], "cp");
+	gr1[1]->SetMarkerStyle(20);
+	gr1[1]->SetLineColor(2);
+	gr1[1]->SetMarkerColor(2);
+	gr1[1]->Draw("same");
 
-    TLegend *w1 = new TLegend(0.35,0.45,0.55,0.65);
+	gr1[2] = new TGraphAsymmErrors();
+	gr1[2]->Divide(hist1[3], hist1[0], "cp");
+	gr1[2]->SetMarkerStyle(20);
+	gr1[2]->SetLineColor(4);
+	gr1[2]->SetMarkerColor(4);
+	gr1[2]->Draw("same");
+
+    TLegend *w1 = new TLegend(0.25,0.25,0.45,0.45);
     w1->SetLineColor(kWhite);
     w1->SetFillColor(0);
-    w1->AddEntry(gr[0], "Gplus+dz","P");
-	w1->AddEntry(gr[1], "Gplus","P");
-	w1->AddEntry(gr[2], "vtx1","P");
+    w1->AddEntry(gr1[2], "Gplus+dz","P");
+	w1->AddEntry(gr1[1], "Gplus","P");
+	w1->AddEntry(gr1[0], "vtx1","P");
 
 	w1->Draw("same");
+
+	c1->cd(2);
+
+	TGraphAsymmErrors * gr2[5];
+	gPad->SetTicks();
+	gPad->SetLeftMargin(0.15);
+	gPad->SetBottomMargin(0.13);
+
+	base2->Draw();
+	gr2[0] = new TGraphAsymmErrors();
+	gr2[0]->Divide(hist2[1], hist2[0], "cp");
+	gr2[0]->SetMarkerStyle(20);
+	gr2[0]->SetLineColor(1);
+	gr2[0]->SetMarkerColor(1);
+	gr2[0]->Draw("same");
+
+	gr2[1] = new TGraphAsymmErrors();
+	gr2[1]->Divide(hist2[2], hist2[0], "cp");
+	gr2[1]->SetMarkerStyle(20);
+	gr2[1]->SetLineColor(2);
+	gr2[1]->SetMarkerColor(2);
+	gr2[1]->Draw("same");
+
+	gr2[2] = new TGraphAsymmErrors();
+	gr2[2]->Divide(hist2[3], hist2[0], "cp");
+	gr2[2]->SetMarkerStyle(20);
+	gr2[2]->SetLineColor(4);
+	gr2[2]->SetMarkerColor(4);
+	gr2[2]->Draw("same");
+
+	c1->SaveAs("../files/pileUpFilterEfficiency_MC_pT.png");
+	c1->SaveAs("../files/pileUpFilterEfficiency_MC_pT.pdf");
+
 
 }
