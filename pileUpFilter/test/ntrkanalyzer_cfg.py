0e9e6d590f626b41f123a44ff2c93b734dfeac69
Kong <kongPro@kongpro.dyndns.cern.ch>
2016-11-17 10:08:24 +0100

    update config

--- pileUpFilter/test/ntrkanalyzer_cfg.py
+++ pileUpFilter/test/ntrkanalyzer_cfg.py
@@ -15,6 +15,21 @@ process.ana_PbPb = cms.EDAnalyzer('NtrkAnalyzer',
 				  offlineptErr = cms.untracked.double(0.1)
 )
 
+pPbPileup = cms.EDAnalyzer('ppPileupFilter',
+    trackSrc = cms.InputTag("generalTracks"),
+    vertexSrc = cms.InputTag("offlinePrimaryVertices"),
+    vertexZMax = cms.double(15.0),
+    ptBins = cms.vdouble(
+        0,0.4,0.5,0.6,0.7,0.8,0.9,1.0,1.1,1.2,1.4,1.6,1.8,2.0,2.2,2.4,3.2,4.0,4.8,5.6,6.4,7.2,9.6,
+        12.0,14.4,19.2,24.0,28.8,35.2,41.6,48.0,60.8,73.6,86.4,103.6,120.8
+    ),
+    qualityString = cms.string("highPurity"),
+    dxySigMax = cms.double(3.0),
+    dzSigMax = cms.double(3.0),
+    ptErrOverptMax = cms.double(0.1)
+)
+
+
 ### standard includes
 process.load('Configuration.StandardSequences.Services_cff')
 process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
@@ -46,7 +61,7 @@ process.ana_PbPb3 = process.ana_PbPb.clone()
 
 process.TFileService = cms.Service("TFileService",fileName = cms.string("Ntrk.root"))
 
-process.p = cms.Path( process.ana_PbPb0 )
-process.p1 = cms.Path( process.pileUpFilter_vtx1*process.ana_PbPb1 )
-process.p2 = cms.Path( process.pileUpFilter_Gplus*process.ana_PbPb2 )
-process.p3 = cms.Path( process.olvFilter_dz1p0*process.ana_PbPb3 )
+process.p = cms.Path( process.ana_PbPb0*process.pPbPileup )
+process.p1 = cms.Path( process.pileUpFilter_vtx1*process.ana_PbPb1*process.pPbPileup )
+process.p2 = cms.Path( process.pileUpFilter_Gplus*process.ana_PbPb2*process.pPbPileup )
+process.p3 = cms.Path( process.olvFilter_dz1p0*process.ana_PbPb3*process.pPbPileup )
