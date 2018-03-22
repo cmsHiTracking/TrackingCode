eeccc6413d290494b52b5db8fbfbeadc031fc1a9
KongTu <kongkong@rice.edu>
2018-03-22 16:35:06 +0100

    fix config of run_pp_cfg

--- HIRun2015Ana/test/run_pp_cfg.py
+++ HIRun2015Ana/test/run_pp_cfg.py
@@ -5,13 +5,13 @@ process.load('Configuration.StandardSequences.Services_cff')
 process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
 process.load('FWCore.MessageService.MessageLogger_cfi')
 process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
-process.load('Configuration.StandardSequences.MagneticField_38T_PostLS1_cff')
 process.load('Configuration.StandardSequences.ReconstructionHeavyIons_cff')
 process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load('TrackingCode.HIRun2015Ana.HITrackCorrectionAnalyzer_cfi')
 
+
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(10)
+    input = cms.untracked.int32(-1)
 )
 
 process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(True))
@@ -28,15 +28,12 @@ process.tpRecoAssocGeneralTracks.label_tr = cms.InputTag("generalTracks")
 process.load("SimTracker.TrackAssociatorProducers.quickTrackAssociatorByHits_cfi")
 process.quickTrackAssociatorByHits.SimToRecoDenominator = cms.string('reco')
 
-process.load("SimTracker.TrackerHitAssociation.clusterTpAssociationProducer_cfi")
-
+process.load("SimTracker.TrackerHitAssociation.tpClusterProducer_cfi")
 # Input source
 process.source = cms.Source("PoolSource",
     duplicateCheckMode = cms.untracked.string("noDuplicateCheck"),
     fileNames =  cms.untracked.vstring(
-##pythia
-#'/store/user/velicanu/MinBias_TuneCUETP8M1_5p02TeV-pythia8/MinBias_TuneCUETP8M1_5p02TeV_pythia8_pp502Fall15_MCRUN2_71_V1_ppreco_RECODEBUG_v0/151117_131129/0000/step3_100.root'
-'/store/user/dgulhan/PYTHIA_QCD_TuneCUETP8M1_cfi_GEN_SIM_5020GeV/PYTHIA_QCD80_TuneCUETP8M1_cfi_RECODEBUGpp_757p1_timeslew_HcalRespCorrs_v4_00_mc/151211_111516/0000/step3_1.root'
+'file:/afs/cern.ch/work/z/ztu/Phase1Upgrade/TimingReductionInPP/default/CMSSW_10_1_0_pre1/src/step3.root'
 )
 )
 ### Track cuts ###
@@ -53,11 +50,11 @@ process.HITrackCorrections.applyTrackCuts = True
 process.HITrackCorrections.fillNTuples = False
 process.HITrackCorrections.applyVertexZCut = True
 process.HITrackCorrections.doVtxReweighting = False
-process.HITrackCorrections.doCaloMatched = True
+process.HITrackCorrections.doCaloMatched = False
 # cut values
-process.HITrackCorrections.dxyErrMax = 3.0
-process.HITrackCorrections.dzErrMax = 3.0
-process.HITrackCorrections.ptErrMax = 0.3 #track trigger 0.1
+process.HITrackCorrections.dxyErrMax = 99.0
+process.HITrackCorrections.dzErrMax = 99.0
+process.HITrackCorrections.ptErrMax = 99.3 #track trigger 0.1
 process.HITrackCorrections.nhitsMin = 0 # track trigger 11
 process.HITrackCorrections.chi2nMax = 9999.9 # track trigger 0.15
 process.HITrackCorrections.reso = 0.2
@@ -66,7 +63,7 @@ process.HITrackCorrections.reso = 0.2
 #pp 5TeV
 process.HITrackCorrections.vtxWeightParameters = cms.vdouble(0.0028637,0.225207,5.61457,0.00296148,0.640783,5.41939)
 ###
-process.HITrackCorrections.algoParameters = cms.vint32(4,5,6,7,8)
+process.HITrackCorrections.algoParameters = cms.vint32(4,5,6,7,8,9,10,11,12,13,14,15,16,20,21,22,23,24,25,26,27,28,29,30)
 process.p = cms.Path(
                       process.tpClusterProducer *
                       process.quickTrackAssociatorByHits *
